71265af574eec15a83b5330a6ed1131631e65c05
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-11-09 14:43:38 -0800

    modified api changes for testing

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,7 @@ const router = express.Router();
 router.post("/createUser", async (req, res) => {
   console.log("req is", req?.body);
   const resp = await db.addUser(req?.body || {});
-  //   console.log("resp is", resp);
-  //   res.status(resp).send({ code: resp });
+  res.status(resp).send({ code: resp });
 });
 
 export default router;
