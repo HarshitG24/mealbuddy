e55d63f2ccc4e07d03709e7020df6bd4e3b56ac8
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-11-11 09:34:30 -0800

    added the header logo

---
+++ frontend/src/components/header/logo.js
@@ -0,0 +1,13 @@
+import logo from "../../images/app_logo.png";
+import "./logo.css";
+
+function Logo() {
+  return (
+    <div className="logo_container">
+      <img src={logo} alt="This is the logo" className="logo_size" />
+      <p className="company_name">MealBuddy</p>
+    </div>
+  );
+}
+
+export default Logo;
