7fe8c532ad8b7ccd5f8b403b1b32fe5abf6293fe
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-11-11 15:03:57 -0800

    added promotional banner

--- frontend/src/components/home/promotion.js
+++ frontend/src/components/home/promotion.js
@@ -4,11 +4,22 @@ import delivery from "../../images/pizza-deliver.png";
 function Promotion() {
   return (
     <div className="promotion_container">
-      <img
-        src={delivery}
-        alt="This is a pizza delivery boy clipart"
-        className="delivery_clipart"
-      />
+      <div className="promotion_image">
+        <img
+          src={delivery}
+          alt="This is a pizza delivery boy clipart"
+          className="delivery_clipart"
+        />
+      </div>
+      <div className="promotion_content">
+        <p className="promotional_title">Hello</p>
+        <p className="promotion_description">
+          Get free delivery on orders above $50
+        </p>
+        <div className="order_now_parent">
+          <button className="order_now">Order Now</button>
+        </div>
+      </div>
     </div>
   );
 }
