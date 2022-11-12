5590b0fe2e39adce695de2b943fcd38cc4db2e9c
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-11-11 16:15:28 -0800

    removed the reusable components from app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,25 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import AddToCart from "./components/addToCart/AddToCart";
-import Logo from "./components/header/logo";
 import Home from "./components/home/home";
-import Navbar from "./components/navbar/Navbar";
-import CreateAccount from "./components/createAccount/createAccount";
 
 function App() {
   const [selected, setSelected] = useState(1);
 
   return (
-    // <div className="container">
-    //   <div className="nav_block">
-    //     <Navbar selected={selected} setSelected={setSelected} />
-    //   </div>
-    //   <div className="content_block">
-    //     <main>
-    //       <Logo />
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/settings" element={<Home />}></Route>
-        <Route path="/createUser" element={<CreateAccount />}></Route>
-      </Routes>
-    </Router>
-    //     </main>
-    //   </div>
-    //   <div className="cart_block">
-    //     <AddToCart />
-    //   </div>
-    // </div>
+    <div className="main_container">
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home selected={selected} setSelected={setSelected} />
+            }></Route>
+          {/* <Route path="/settings" element={<Home />}></Route>
+          <Route path="/createUser" element={<CreateAccount />}></Route> */}
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
