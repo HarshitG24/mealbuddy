0bad4e6d1ba7d6a3b1951ce711b2e65cce871327
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-11-13 17:29:04 -0800

    made the cart order as a seperate component

--- frontend/src/components/addToCart/AddToCart.js
+++ frontend/src/components/addToCart/AddToCart.js
@@ -1,10 +1,7 @@
 import "./css/add-to-cart.css";
-import "./css/cart-order.css";
 import "../home/css/product.css";
 import delivery from "../../images/delivery.png";
-import sandwich from "../../images/sandwich.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import CartOrder from "./CartOrder";
 
 function AddToCart() {
   return (
@@ -20,27 +17,7 @@ function AddToCart() {
         <p>Your Order</p>
 
         {/* Cart orders */}
-        <div className="cart_order_container">
-          <img src={sandwich} alt="This is the icon for product added" />
-          <div className="cart_product_left">
-            <p className="cart_product_name">Spicy Chicken Ranch</p>
-            <div className="cart_quantity">
-              <button className="cart_btns">-</button>
-              <p className="item_quantity">1</p>
-              <button className="cart_btns">+</button>
-
-              <FontAwesomeIcon
-                icon={faTrash}
-                size="1x"
-                className="add_to_cart_delete"
-                onClick={(e) => {}}
-              />
-            </div>
-          </div>
-          <div className="add_to_cart_price">
-            <p>$12</p>
-          </div>
-        </div>
+        <CartOrder />
       </div>
     </div>
   );
