1c8de734b2bf8bd4efe2d100fa491c282a72637c
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-11-13 17:27:30 -0800

    added the component to show orders in cart

--- frontend/src/components/addToCart/AddToCart.js
+++ frontend/src/components/addToCart/AddToCart.js
@@ -1,6 +1,10 @@
 import "./css/add-to-cart.css";
 import "./css/cart-order.css";
+import "../home/css/product.css";
 import delivery from "../../images/delivery.png";
+import sandwich from "../../images/sandwich.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function AddToCart() {
   return (
@@ -16,7 +20,27 @@ function AddToCart() {
         <p>Your Order</p>
 
         {/* Cart orders */}
-        <div className="cart_order_container"></div>
+        <div className="cart_order_container">
+          <img src={sandwich} alt="This is the icon for product added" />
+          <div className="cart_product_left">
+            <p className="cart_product_name">Spicy Chicken Ranch</p>
+            <div className="cart_quantity">
+              <button className="cart_btns">-</button>
+              <p className="item_quantity">1</p>
+              <button className="cart_btns">+</button>
+
+              <FontAwesomeIcon
+                icon={faTrash}
+                size="1x"
+                className="add_to_cart_delete"
+                onClick={(e) => {}}
+              />
+            </div>
+          </div>
+          <div className="add_to_cart_price">
+            <p>$12</p>
+          </div>
+        </div>
       </div>
     </div>
   );
