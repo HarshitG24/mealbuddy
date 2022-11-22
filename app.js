8b0f0d66cea2a15694b199e7abfddc14a26d5845
Mihir <mesiamihir@gmail.com>
2022-11-21 17:50:05 -0800

    updates

--- app.js
+++ app.js
@@ -45,10 +45,7 @@ app.use("/api/home", dataRouter);
 app.use("/api/pizza", PizzaRouter);
 
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    process.env.DEP_LINK || "http://localhost:3000"
-  );
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
