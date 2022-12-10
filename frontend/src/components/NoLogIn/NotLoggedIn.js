a6471d1a4e46d874129a8dd9c4c366c7457a3e31
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-12-09 19:52:20 -0800

    added the fonts on settings page

--- frontend/src/components/NoLogIn/NotLoggedIn.js
+++ frontend/src/components/NoLogIn/NotLoggedIn.js
@@ -1,8 +1,9 @@
 // AUTHOR: HARSHIT GAJJAR
+import "./notloggedin.css";
 
 function NotLoggedIn() {
   return (
-    <div>
+    <div className="no_login_container">
       <h1>Please Login To continue</h1>
     </div>
   );
